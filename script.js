a1e9fd854acef6579e7e910ba40f2d741d25d423
sasefa <131699944+sasefa@users.noreply.github.com>
2023-06-22 00:13:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Assignment code here
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -9,12 +8,10 @@ document.addEventListener("DOMContentLoaded", function () {
     let includeSpecial;
     let length;
   
-    // Select elements
     const generateBtn = document.getElementById("generateBtn");
     const recreateBtn = document.getElementById("recreateBtn");
     const passwordField = document.getElementById("password");
   
-    // Generate a new password
     function generatePassword() {
       includeLower = confirm("Include lowercase letters?");
       includeUpper = confirm("Include uppercase letters?");
@@ -22,12 +19,10 @@ document.addEventListener("DOMContentLoaded", function () {
       includeSpecial = confirm("Include special characters?");
       length = prompt("Enter desired password length (between 8 and 128):");
   
-      // Ensure that length is a number between 8 and 128
       while (isNaN(length) || length < 8 || length > 128) {
         length = prompt("Invalid length. Please enter a number between 8 and 128:");
       }
   
-      // Define character sets based on user prompts
       const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
       const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
       const numberChars = "0123456789";
@@ -47,10 +42,8 @@ document.addEventListener("DOMContentLoaded", function () {
         charSet += specialChars;
       }
   
-      // Initialize password
       let password = "";
   
-      // Fill password with random characters from selected character sets
       for (let i = 0; i < length; i++) {
         const random = Math.floor(Math.random() * charSet.length);
         password += charSet[random];
@@ -59,14 +52,12 @@ document.addEventListener("DOMContentLoaded", function () {
       return password;
     }
   
-    // Generate a new password and display it
     generateBtn.addEventListener("click", function () {
       const password = generatePassword();
       passwordField.value = password;
       recreateBtn.style.display = "block";
     });
   
-    // Generate a new password with the previously selected criteria
     recreateBtn.addEventListener("click", function () {
       const password = generatePassword();
       passwordField.value = password;
